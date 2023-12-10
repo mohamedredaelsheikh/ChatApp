9ca80361d10af6b1d11adb26c236fe11fe2bbfcd
Mohammed_Elsheikh <moh.reda198@gmail.com>
2023-12-10 22:58:33 +0200

    Create Auth Bloc and simple bloc observer

--- lib/features/AuthBloc/auth_bloc.dart
+++ lib/features/AuthBloc/auth_bloc.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/login_page.dart';
 import 'package:chat_app/pages/register_page.dart';
+import 'package:chat_app/simpleblocobserver.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,6 +14,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  Bloc.observer = AppBlocObserver();
   runApp(const ScholarChat());
 }
 
